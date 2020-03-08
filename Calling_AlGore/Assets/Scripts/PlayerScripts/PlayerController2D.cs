11b0f477c426d18d0ede6a7887a9442fcc773dda
mark Sahlgreen <mark@nozzy.org>
2020-03-08 16:58:59 +0100

    what he said

--- Calling_AlGore/Assets/Scripts/PlayerScripts/PlayerController2D.cs
+++ Calling_AlGore/Assets/Scripts/PlayerScripts/PlayerController2D.cs
@@ -7,6 +7,10 @@ public class PlayerController2D : MonoBehaviour
     Animator animator;
     Rigidbody2D rb2d;
     SpriteRenderer spriteRenderer;
+    [SerializeField]
+    Transform groundCheck;
+
+    bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -18,17 +22,28 @@ public class PlayerController2D : MonoBehaviour
 
     private void FixedUpdate()
     {   
+        // Check to see if character is on the ground
+        if (Physics2D.Linecast(transform.position, groundCheck.position, 
+            1 << LayerMask.NameToLayer("Ground"))) {
+            isGrounded = true;
+        }
+        else {
+            isGrounded = false;
+        }
+
         // Basic player movement based on input
-        if(Input.GetKey("d") || Input.GetKey("right"))
-        {
+        if (Input.GetKey("d") || Input.GetKey("right")) {
             rb2d.velocity = new Vector2(2, rb2d.velocity.y);
         }
-        else if (Input.GetKey("a") || Input.GetKey("left"))
-        {
+        else if (Input.GetKey("a") || Input.GetKey("left")) {
             rb2d.velocity = new Vector2(-2, rb2d.velocity.y);
         }
+        // Stop if not going right or left
+        else {
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+        }
         // Jumping
-        if (Input.GetKey("space") || Input.GetKey("up"))
+        if (Input.GetKey("space") && isGrounded)
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, 4);
         }
