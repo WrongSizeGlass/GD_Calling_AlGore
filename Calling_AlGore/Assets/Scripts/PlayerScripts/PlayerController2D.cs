73c4c93330c93d0f1a80bdf0c86abe4481f178e0
mark Sahlgreen <mark@nozzy.org>
2020-03-08 15:41:09 +0100

    the cube can now go left adn right with a and d or left and right. can also fly with space and up

---
+++ Calling_AlGore/Assets/Scripts/PlayerScripts/PlayerController2D.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D rb2d;
+    SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2d.velocity = new Vector2(2, 0);
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+
+            rb2d.velocity = new Vector2(-2, 0);
+        }
+        else if (Input.GetKey("space") || Input.GetKey("up"))
+        {
+
+            rb2d.velocity = new Vector2(0, 4);
+        }
+    }
+
+}
