8ac08d4231fa828a4e6d79543d9506bb621de70c
Zoffer <38067527+WrongSizeGlass@users.noreply.github.com>
2020-03-09 22:38:33 +0100

    Camera Script added

---
+++ Calling_AlGore/Assets/Scripts/Camera/CameraScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private Vector3 offSet;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offSet = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        constrainCamera();
+    }
+
+    void constrainCamera (){
+        float newXPos = player.transform.position.x - offSet.x;
+        float newYPos = player.transform.position.y - offSet.y;
+
+        transform.position = new Vector3(newXPos, newYPos, transform.position.z);
+    }
+}
--- Calling_AlGore/Assets/Scripts/PlayerScripts/PlayerController/PlayerController2D.cs
+++ Calling_AlGore/Assets/Scripts/PlayerScripts/PlayerController/PlayerController2D.cs
@@ -4,48 +4,52 @@ using UnityEngine;
 
 public class PlayerController2D : MonoBehaviour
 {
-    Animator animator;
     Rigidbody2D rb2d;
-    SpriteRenderer spriteRenderer;
+
     [SerializeField]
     Transform groundCheck;
 
+    int speed = 3;
+    int jumpHeight = 5;
     bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
-    {   
+    {
         // Check to see if character is on the ground
-        if (Physics2D.Linecast(transform.position, groundCheck.position, 
-            1 << LayerMask.NameToLayer("Ground"))) {
+        if (Physics2D.Linecast(transform.position, groundCheck.position,
+            1 << LayerMask.NameToLayer("Ground")))
+        {
             isGrounded = true;
         }
-        else {
+        else
+        {
             isGrounded = false;
         }
 
         // Basic player movement based on input
-        if (Input.GetKey("d") || Input.GetKey("right")) {
-            rb2d.velocity = new Vector2(2, rb2d.velocity.y);
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
         }
-        else if (Input.GetKey("a") || Input.GetKey("left")) {
-            rb2d.velocity = new Vector2(-2, rb2d.velocity.y);
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2d.velocity = new Vector2(-speed, rb2d.velocity.y);
         }
         // Stop if not going right or left
-        else {
+        else
+        {
             rb2d.velocity = new Vector2(0, rb2d.velocity.y);
         }
         // Jumping
         if (Input.GetKey("space") && isGrounded)
         {
-            rb2d.velocity = new Vector2(rb2d.velocity.x, 4);
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpHeight);
         }
     }
 
