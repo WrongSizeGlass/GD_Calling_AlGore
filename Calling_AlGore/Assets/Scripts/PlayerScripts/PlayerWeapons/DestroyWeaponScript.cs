44cec56d9e421e642a38121acf85f55a1de661d7
unknown <xmantissx@gmail.com>
2020-03-10 14:46:37 +0100

    build weapon

--- Calling_AlGore/Assets/Scripts/PlayerScripts/PlayerWeapons/DestroyWeaponScript.cs
+++ Calling_AlGore/Assets/Scripts/PlayerScripts/PlayerWeapons/DestroyWeaponScript.cs
@@ -6,23 +6,49 @@ public class DestroyWeaponScript : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefrab;
+    destroyBulletScript a;
+    float time;
+    float timer;
+    bool timerStarted;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = 1.0f;
+        timerStarted = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1")){
+        if(Input.GetButtonUp("Fire1") && timerStarted){
             shoot();
+            
         }
+        
+        if(timerStarted){
+            time += Time.deltaTime;
+            if(time > timer){
+                shoot();
+                
+            }
+        }
+        
+        
+
+        if(Input.GetButtonDown("Fire1")){
+            timerStarted = true;
+        }
+        
     }
 
     void shoot(){
 
+        a = bulletPrefrab.GetComponent<destroyBulletScript>();
+        a.force = 3.0f + (time * 10.0f);
         Instantiate(bulletPrefrab, firePoint.position, firePoint.rotation);
+        Debug.Log(a.force);
+        time = 0;
+        timerStarted = false;
 
     }
 
--- Calling_AlGore/Assets/Scripts/PlayerScripts/PlayerWeapons/destroyBulletScript.cs
+++ Calling_AlGore/Assets/Scripts/PlayerScripts/PlayerWeapons/destroyBulletScript.cs
@@ -4,27 +4,52 @@ using UnityEngine;
 
 public class destroyBulletScript : MonoBehaviour
 {
-
-    private float speed = 5f;
     private int dmg = 1;
+    public GameObject buildCube;
     Rigidbody2D rb;
+    public float force;
+    Vector2 Acceleration;
+    Vector2 gravity;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = transform.right * speed;
+        gravity = Physics2D.gravity * rb.gravityScale * Vector2.up;
+        Acceleration = new Vector2(force,force);
+        rb.velocity = Acceleration;
     }
 
-    private void OnTriggerEnter2D(Collider2D hit)
+    void FixedUpdate()
     {
-        DieScript enemy = hit.GetComponent<DieScript>();
+
+        if(force <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        rb.AddForce(gravity);
+        
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D hit)
+    {
+       /* DieScript enemy = hit.GetComponent<DieScript>();
         if(enemy != null){
             enemy.TakeDamage(dmg);
         }
       //  Debug.Log(hit);
         Destroy(gameObject);
+        */
+       //Vector2 spawnpos = new Vector2(transform.position.x,transform.position.y);
+
+      
+            //Vector2 spawnpos = new Vector2(transform.position.x,transform.position.y);
+            Instantiate(buildCube,new Vector2(transform.position.x, transform.position.y + 0.5f) , transform.rotation);
+            Destroy(gameObject);
+        
     }
 
     // Update is called once per frame
-
 }
